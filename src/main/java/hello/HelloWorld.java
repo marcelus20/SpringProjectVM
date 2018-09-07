400710c68233360ad4d36045cc71c86972fa4d9e
felipe <marcelus20felipe@gmail.com>
2018-09-07 23:23:04 +0100

    added one more component scan (persistence)

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -3,11 +3,21 @@ package hello;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import persistence.entity.AccUser;
+import persistence.entity.TransactionType;
+import persistence.entityManager.MyEntityManager;
+
+import java.util.List;
 
 @SpringBootApplication
-@ComponentScan({"controllers"})
+@ComponentScan({"controllers", "persistence"})
 public class HelloWorld {
+
     public static void main(String... args){
+
         SpringApplication.run(HelloWorld.class, args);
+
+
     }
+
 }
